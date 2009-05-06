7029b3efde796e17115b32b7f9af4cc9e49a8502
danfreak <dan@freakclimbing.com>
2009-05-07 01:37:25 +0200

    resize to given dimensions + crop

--- mycommon/image_kit.py
+++ mycommon/image_kit.py
@@ -3,7 +3,7 @@ from google.appengine.api import images
 
 def flickr_thumb(file,size):
   """ 
-  returns squared thumbnail
+  returns squared cropped thumbnail
   """
   width,height =  getImageInfo(file)
   
@@ -16,12 +16,15 @@ def flickr_thumb(file,size):
     file  =  images.resize(file,height=size)
     width,height =  getImageInfo(file)
     cropoff =  ((width  -  size)*.5)/width
-    file  = images.crop(file,left_x=cropoff, right_x=1.0 - cropoff,top_y=0.0,bottom_y=1.0) 
+    file  = images.crop(file,left_x=cropoff, right_x=1.0 - cropoff,top_y=0.0,bottom_y=1.0)
+    
+  file = images.im_feeling_lucky(file)
   return file
 
 def resize_to_max(file, max_w, max_h):
   """ 
   rezizes an image in order to fit max dimensions keeping proportions
+  NO crop
   """
   
   width, height = getImageInfo(file)
@@ -47,28 +50,38 @@ def resize_to_max(file, max_w, max_h):
   file = images.resize(file, new_w, new_h)
   return file
 
+
 def resize_to(file, w, h):
+
   """ 
   rezizes an image to given dimensions
+  cropping it in order to keep proportions
   """
   width, height = getImageInfo(file)
-  
-  if width < height:
-    file  =  images.resize(file,height=h)
-    width,height =  getImageInfo(file)
-    cropoff =  ((width  -  w)*.5)/width
-    file =  images.crop(file,top_y=0.0, bottom_y=1.0  - cropoff,left_x=cropoff,right_x=1.0  - cropoff)
+  #ratio = width/height
+  #ratio_n = w/h
+  #ratio_new = ratio/ratio_n
+  w_new = height * (w/h)
+  h_new = width / (w/h)
+  #cut width
+  if w_new < width:
+    
+    cropoff =  (abs(width  -  w_new)*.5)/width
+    file =  images.crop(file,top_y=0.0, bottom_y=1.0,left_x=cropoff,right_x=1.0  - cropoff)
+    file = images.resize(file, height= h)
+  #cut height
+  elif h_new < height:
+    
+    #print h_new
+    cropoff =  (abs(height  -  h_new)*.5)/height
+    file =  images.crop(file,top_y=cropoff, bottom_y=1.0 - cropoff,left_x=0.0,right_x=1.0 )
+    file = images.resize(file, width= w)
   else:
-    file  =  images.resize(file,width=w)
-    width,height =  getImageInfo(file)
-    cropoff =  ((height  -  h)*.5)/height
-    file =  images.crop(file,top_y=cropoff, bottom_y=1.0  - cropoff,left_x=0.0,right_x=1.0)
-  """
-  file = images.resize(file, width= w, height = h)
-  """
+    file = images.resize(file, width = w, height= h)
+    
+  #file = images.im_feeling_lucky(file)
   return file
   
-
 def getImageInfo(file):
   """ 
   returns image dimensions
